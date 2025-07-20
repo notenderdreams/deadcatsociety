cf82b93443aa7a14db17d4ec423d663e8b543c50
enderdreams <enderdreams@outlook.com>
2025-07-21 00:34:50 +0600

    Feat: A Sleeping cat in the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { TextEffect } from "@/components/motion-primitives/text-effect";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-neutral-100">
       <div className="text-center">
-        <div
-          style={{ fontFamily: "FreightDispPro, serif" }}
-          className="text-5xl italic text-[#3d3d3d]"
-        >
-          .deadcatsociety
+        <div className="flex ">
+          <div
+            style={{ fontFamily: "FreightDispPro, serif" }}
+            className="text-5xl italic text-[#3d3d3d]"
+          >
+            .deadcatsociety
+          </div>
+          <div className="mt-[-16px]">
+            <Image
+              unoptimized={true}
+              src="/sleeping_cat.gif"
+              alt="the gif"
+              height={64}
+              width={64}
+            />
+          </div>
         </div>
 
         <div className="font-sans font-medium  text-[#3d3d3d] mt-2">
