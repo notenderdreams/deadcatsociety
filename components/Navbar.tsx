267b68b23af7a79aa424c767f210ae053e72b2f7
enderdreams <enderdreams@outlook.com>
2025-07-21 01:00:20 +0600

    Fix: Navbar
    Fixes:
    - icons
    - Tab Names
    
    Note: Note sure about the name of the chatbot

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { Home, Fan, Bolt, Package } from "lucide-react";
+import { Home, Fan, Bolt, Calendar, Sparkles, Notebook } from "lucide-react";
 import { ExpandedTabs } from "@/components/ui/expanded-tabs";
 
 function Navbar() {
   const tabs = [
     { title: "Home", icon: Home, path: "/" },
-    { title: "Notes", icon: Home, path: "/projects" },
-    { title: "Batman", icon: Package, path: "/crates" },
-    { type: "separator" as const },
+    { title: "Notes", icon: Notebook, path: "/projects" },
+    { title: "Askity", icon: Sparkles , path: "/crates" },
+    { title: "Events", icon: Calendar, path: "/crates" },
     { title: "Settings", icon: Bolt, path: "/settings" },
     { title: "About", icon: Fan, path: "/about" },
   ];
--- components/ui/expanded-tabs.tsx
+++ components/ui/expanded-tabs.tsx
@@ -117,7 +117,7 @@ export function ExpandedTabs({
               "relative flex items-center gap-2 rounded-xl px-4 py-2 text-sm transition-colors duration-300 z-10",
               isSelected
                 ? "text-[#808080] font-bold"
-                : "text-[#A0A0A0] hover:bg-zinc-700/50 hover:text-white font-medium"
+                : "text-[#A0A0A0] hover:bg-zinc-500/50 hover:text-white font-medium"
             )}
           >
             <Icon size={20} color={isSelected ? "#808080" : "#A0A0A0"} />
